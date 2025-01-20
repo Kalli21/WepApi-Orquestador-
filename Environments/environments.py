04f2b942694ad7aa18cb1aa2bbf19703f2141a53
Kalli <kalli_28456@hotmail.com>
2025-01-20 01:07:56 -0500

    update enviroments

--- Environments/environments.py
+++ Environments/environments.py
@@ -1,3 +1,7 @@
-NET_API_BASE_URL    = "https://localhost:7015/api"
-apiClasModelUrl     = 'http://127.0.0.1:8003'
-apiTopicModelUrl    = 'http://127.0.0.1:8004'
+# NET_API_BASE_URL    = "https://localhost:7015/api"
+# apiClasModelUrl     = 'http://127.0.0.1:8003'
+# apiTopicModelUrl    = 'http://127.0.0.1:8004'
+# -----------------------------------------------
+NET_API_BASE_URL    = "https://analisissentimientoback-a8dxe7c2hegabebg.canadacentral-01.azurewebsites.net"
+apiClasModelUrl     = 'http://52.191.250.120'
+apiTopicModelUrl    = 'http://20.115.85.253'
