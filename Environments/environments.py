5f8505ea56cb0c2b04333c0416bb04d944b54b2d
Kalli <kalli_28456@hotmail.com>
2025-01-21 01:31:55 -0500

    conexiones

--- Environments/environments.py
+++ Environments/environments.py
@@ -2,6 +2,6 @@
 # apiClasModelUrl     = 'http://127.0.0.1:8003'
 # apiTopicModelUrl    = 'http://127.0.0.1:8004'
 # -----------------------------------------------
-NET_API_BASE_URL    = "https://analisissentimientoback-a8dxe7c2hegabebg.canadacentral-01.azurewebsites.net"
+NET_API_BASE_URL    = "https://analisissentimientoback-a8dxe7c2hegabebg.canadacentral-01.azurewebsites.net/api"
 apiClasModelUrl     = 'http://52.191.250.120'
 apiTopicModelUrl    = 'http://20.115.85.253'
