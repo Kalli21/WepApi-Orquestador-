0d1612cda818176ea324d29f8814037eac1e878c
Kalli <kalli_28456@hotmail.com>
2025-01-24 02:35:03 -0500

    Configuraion apis

--- Environments/environments.py
+++ Environments/environments.py
@@ -3,5 +3,5 @@
 # apiTopicModelUrl    = 'http://127.0.0.1:8004'
 # -----------------------------------------------
 NET_API_BASE_URL    = "https://analisissentimientoback-a8dxe7c2hegabebg.canadacentral-01.azurewebsites.net/api"
-apiClasModelUrl     = 'https://52.191.250.120'
-apiTopicModelUrl    = 'https://20.115.85.253'
+apiClasModelUrl     = 'http://52.191.250.120'
+apiTopicModelUrl    = 'http://20.115.85.253'
